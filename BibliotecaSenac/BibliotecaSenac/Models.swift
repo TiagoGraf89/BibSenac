b0ed7085ab7339959380e4685466071170d7ba37
Tiago <tiago.graf@gmail.com>
2016-11-19 15:51:23 -0200

    Adicionado a Primeira Versao da Estrutura do Projeto e Model

---
+++ BibliotecaSenac/BibliotecaSenac/Models.swift
@@ -0,0 +1,45 @@
+//
+//  Models.swift
+//  BibliotecaSenac
+//
+//  Created by Tiago on 11/19/16.
+//  Copyright © 2016 Tiago. All rights reserved.
+//
+
+import UIKit
+
+class ItemModel: NSObject {
+    override init() {
+        self._id = 0
+        self._senac = 0
+        self.aArtg = ""
+        self.aPrin = ""
+        self.dstag = ""
+        self.chamd = ""
+        self.aScdr = ""
+        self.pbcao = ""
+        self.fsico = ""
+        self.rsumo = ""
+        self.notas = ""
+        self.assun = ""
+        self.url = ""
+        self.pdico = ""
+        self.serie = ""
+    }
+    
+    var _id: Int
+    var _senac: Int
+    var dstag: String
+    var chamd: String
+	var aPrin: String
+	var aArtg: String
+	var aScdr: String
+    var pbcao: String
+    var fsico: String
+	var rsumo: String
+	var notas: String
+	var assun: String
+	var url: String
+	var pdico: String
+	var serie: String
+}
---
+++ BibliotecaSenac/BibliotecaSenac/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  BibliotecaSenac
+//
+//  Created by Tiago on 11/19/16.
+//  Copyright © 2016 Tiago. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+}
